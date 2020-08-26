9730402ee60bba3dd75531cb3d53f29f5dad9ec3
Deep Majumder <deep.majumder2019@gmail.com>
2020-08-26 20:50:29 +0530

    Added command line args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use decision_tree;
 use decision_tree::InputData;
+use std::env;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let data_filename = "/home/deep/work/rust/decision_tree/data/mushroom.csv";
-    let data = InputData::from_file(data_filename, 0)?;
-    println!("{}", data);
-    let tree = decision_tree::ida3(&data);
-    println!("{}", tree);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} <csv_file_path> <result_column_number>", args[0]);
+    } else {
+        let data_filename = &args[1];
+        let result_column: usize = args[2].parse()?;
+        let data = InputData::from_file(data_filename, result_column)?;
+        let tree = decision_tree::ida3(&data);
+        println!("{}", tree);
+    }
     Ok(())
 }
